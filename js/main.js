d5af2b5fff1f88544049e1ab5412f529288be212
Manjeet <43195111+Manjeete@users.noreply.github.com>
2020-01-03 23:58:24 +0530

    fixed the the toggle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
   $("#left_arrow").click(function(){
-        $("#leftPortal").hide();
+  		$("#leftPortal").toggleClass("col-md-2");
+        $("#leftPortal").toggleClass("togLeftDiv");
+        $("#left_arrow").toggleClass("angleLeft");
+
+        $("img").toggleClass('imgToggle');
+        $("span,#leftDash,ul,#dropLegal").toggle();
+
 	});
 
 
