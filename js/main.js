d29505829b26264ad1fe307cc640ed5b1cb878df
Manjeet <43195111+Manjeete@users.noreply.github.com>
2020-01-03 22:56:59 +0530

    added jquery to toggle left part

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#left_arrow").click(function(){
+        $("#leftPortal").hide();
+	});
+
+
+  $(".fa-angle-down").click(function(){
+  	$('li').toggle();
+  });
+});
