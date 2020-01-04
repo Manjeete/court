93aa9c31ee4860a97fb02c98fb8ce3de9a7d0f3c
Manjeet <43195111+Manjeete@users.noreply.github.com>
2020-01-04 07:38:05 +0530

    remove the balance image and add balance scale icon

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   		$("#leftPortal").toggleClass("col-md-2");
         $("#leftPortal").toggleClass("togLeftDiv");
         $("#left_arrow").toggleClass("angleLeft");
-
+        $('.fa-balance-scale').toggleClass("balanceScale");
         $("img").toggleClass('imgToggle');
         $("span,#leftDash,ul,#dropLegal").toggle();
 
